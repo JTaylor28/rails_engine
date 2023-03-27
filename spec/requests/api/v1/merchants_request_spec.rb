640aa859e3c8b29a2e711096975b34547411b4cf
James Taylor <jrtaylor0614@gmail.com>
2023-03-27 17:56:58 -0600

    Test: Modle/Controller

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants api' do
+  it "sneds a list of Merchants" do
+    create_list(:merchant, 3)
+    
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants.count).to eq(3)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a(Integer)
+
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
+    end
+  end
+end
