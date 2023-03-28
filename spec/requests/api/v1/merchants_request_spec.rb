42676c3c6c13259bd03b734b05d8fa1dad8fbc77
James Taylor <jrtaylor0614@gmail.com>
2023-03-27 22:12:26 -0600

    Test: merchant controller #show endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe 'Merchants api' do
+RSpec.describe 'Merchants api', type: :request  do
+ 
   before do 
     create_list(:merchant, 3)
   end
 
   describe "#index" do 
+
     before do  
       get '/api/v1/merchants'
     end
@@ -15,12 +17,50 @@ RSpec.describe 'Merchants api' do
         
         expect(response).to be_successful
 
-        merchants = JSON.parse(response.body, symbolize_names: true)
+        parsed_data = JSON.parse(response.body, symbolize_names: true)
 
-        expect(merchants[:data].count).to eq 3
-        expect(merchants[:data][0].keys).to eq([:id, :type, :attributes])
-        expect(merchants[:data][0][:attributes][:name]).to eq(Merchant.first.name)
+        expect(parsed_data[:data].count).to eq(3)
+        expect(parsed_data[:data][0].keys).to eq([:id, :type, :attributes])
+        expect(parsed_data[:data][0][:attributes][:name]).to eq(Merchant.first.name)
       end
     end
   end 
+
+  describe "#show" do
+    
+    context "when successful" do
+      
+      before do
+        get "/api/v1/merchants/#{Merchant.first.id}"
+      end
+
+      it "returns a specific merchant" do
+
+        expect(response).to be_successful
+
+        parsed_data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_data[:data].count).to eq(3)
+        expect(parsed_data[:data][:attributes].count).to eq(1)
+        expect(parsed_data[:data][:id]).to eq(Merchant.first.id.to_s)
+        expect(parsed_data[:data][:attributes][:name]).to eq(Merchant.first.name)
+      end
+    end
+
+    context "when unsuccessful" do
+
+      before do
+        get "/api/v1/merchants/abc"
+      end
+
+      it "returns an error message" do 
+        
+        expect(response).to have_http_status(404)
+
+        parsed_data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_data[:message]).to eq("Couldn't find Merchant with 'id'=abc")
+      end
+    end
+  end
 end
