e933ab756a8fda808785f17723f56e5ec27fc468
James Taylor <jrtaylor0614@gmail.com>
2023-03-27 22:35:33 -0600

    Test: refactor MerchantController #show

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -40,9 +40,10 @@ RSpec.describe 'Merchants api', type: :request  do
 
         parsed_data = JSON.parse(response.body, symbolize_names: true)
 
-        expect(parsed_data[:data].count).to eq(3)
-        expect(parsed_data[:data][:attributes].count).to eq(1)
+        expect(parsed_data[:data].keys).to eq([:id, :type, :attributes])
+        expect(parsed_data[:data][:attributes].size).to eq(1)
         expect(parsed_data[:data][:id]).to eq(Merchant.first.id.to_s)
+        expect(parsed_data[:data][:type]).to eq("merchant")
         expect(parsed_data[:data][:attributes][:name]).to eq(Merchant.first.name)
       end
     end
@@ -54,7 +55,7 @@ RSpec.describe 'Merchants api', type: :request  do
       end
 
       it "returns an error message" do 
-        
+
         expect(response).to have_http_status(404)
 
         parsed_data = JSON.parse(response.body, symbolize_names: true)
