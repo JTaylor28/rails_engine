e522d7c8a159305f1ef1a734c0eef287572dd130
James Taylor <jrtaylor0614@gmail.com>
2023-03-28 16:43:29 -0600

    Create model tests adds model tests for all tables and relevant associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
 
-  validates_presence_of :name, :description, :unit_price
-  validates_numericality_of :unit_price
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :unit_price, presence: true, numericality: true
+  validates :merchant_id, presence: true
 end
