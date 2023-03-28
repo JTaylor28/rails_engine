ceb1371d1c21ba475813a2c5f013fb8b177779c5
James Taylor <jrtaylor0614@gmail.com>
2023-03-28 11:04:43 -0600

    Test: Items controller #show

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -28,4 +28,46 @@ RSpec.describe 'items api', type: :request  do
       end
     end
   end 
-end 
+
+  describe "#show" do
+    
+    context "when successful" do
+      
+      before do
+        get "/api/v1/items/#{Item.first.id}"
+      end
+
+      it "returns a specific Item" do
+
+        expect(response).to be_successful
+
+        parsed_data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_data[:data].keys).to eq([:id, :type, :attributes])
+        expect(parsed_data[:data][:attributes].size).to eq(4)
+        expect(parsed_data[:data][:id]).to eq(Item.first.id.to_s)
+        expect(parsed_data[:data][:type]).to eq("item")
+        expect(parsed_data[:data][:attributes][:name]).to eq(Item.first.name)
+        expect(parsed_data[:data][:attributes][:description]).to eq(Item.first.description)
+        expect(parsed_data[:data][:attributes][:unit_price]).to eq(Item.first.unit_price)
+        expect(parsed_data[:data][:attributes][:merchant_id]).to eq(Item.first.merchant_id)
+      end
+    end
+
+    context "when unsuccessful" do
+
+      before do
+        get "/api/v1/items/1a2d3"
+      end
+
+      it "returns an error message" do 
+
+        expect(response).to have_http_status(404)
+
+        parsed_data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_data[:message]).to eq("Couldn't find Item with 'id'=1a2d3")
+      end
+    end
+  end
+end
