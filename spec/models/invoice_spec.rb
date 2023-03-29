6ba7129485691c61d210be2d797c2920c214114b
James Taylor <jrtaylor0614@gmail.com>
2023-03-29 16:46:49 -0600

    test: instance method #items_present

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -7,4 +7,29 @@ RSpec.describe Invoice, type: :model do
     it { should have_many :invoice_items }
     it { should have_many(:items).through(:invoice_items) }
   end
+
+  describe 'instance methods' do 
+    describe '#items_present?' do
+      it "retuens false if invoice have has one or less " do
+
+        merchant = create(:merchant)
+				customer = create(:customer)
+
+				item1 = create(:item, merchant_id: merchant.id)
+				item2 = create(:item, merchant_id: merchant.id)
+
+				invoice1 = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+				invoice2 = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+
+				invoice_item1 = create(:invoice_item, invoice_id: invoice1.id, item_id: item1.id, quantity: 1)
+				invoice_item2 = create(:invoice_item, invoice_id: invoice1.id, item_id: item2.id, quantity: 3)
+				invoice_item3 = create(:invoice_item, invoice_id: invoice2.id, item_id: item1.id, quantity: 2)
+
+				expect(invoice1.items.size).to eq(2)
+				expect(invoice1.items_present?).to eq(false)
+				expect(invoice2.items.size).to eq(1)
+				expect(invoice2.items_present?).to eq(true)
+      end
+    end
+  end
 end
