17131086c9293bd1913b583c06dd12fd19c831a6
James Taylor <jrtaylor0614@gmail.com>
2023-03-29 16:49:02 -0600

    feat: #items_present

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,13 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
-  has_many :transactions
-  has_many :invoice_items
-  has_many :items, through: :invoice_items
+  has_many :transactions, dependent: :destroy
+  has_many :invoice_items, dependent: :destroy
+  has_many :items, through: :invoice_items, dependent: :destroy
+
+
+  def items_present?
+    self.items.size <= 1
+  end
+  
 end
