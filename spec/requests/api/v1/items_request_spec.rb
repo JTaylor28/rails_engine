d65aadd105adba7ae9883518e577f6b8f4d33759
James Taylor <jrtaylor0614@gmail.com>
2023-03-28 18:22:38 -0600

    Refactor: change items controller test for thoroughness

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -15,16 +15,18 @@ RSpec.describe 'items api', type: :request  do
     context 'when successful' do
 
       it "returns all items" do
-        
         expect(response).to be_successful
 
         parsed_data = JSON.parse(response.body, symbolize_names: true)
 
+        expect(parsed_data[:data]).to be_an(Array)
         expect(parsed_data[:data].size).to eq(3)
         expect(parsed_data[:data][0].keys).to eq([:id, :type, :attributes])
+        expect(parsed_data[:data][0][:attributes].size).to eq(4)
         expect(parsed_data[:data][0][:attributes][:name]).to eq(Item.first.name)
         expect(parsed_data[:data][0][:attributes][:description]).to eq(Item.first.description)
         expect(parsed_data[:data][0][:attributes][:unit_price]).to eq(Item.first.unit_price)
+        expect(parsed_data[:data][0][:attributes][:unit_price]).to be_a(Float)
         expect(parsed_data[:data][0][:attributes][:merchant_id]).to eq(Item.first.merchant_id)
       end
     end
@@ -35,7 +37,8 @@ RSpec.describe 'items api', type: :request  do
     context "when successful" do
       
       before do
-        get "/api/v1/items/#{Item.first.id}"
+        @show_item = Item.first
+        get "/api/v1/items/#{@show_item.id}"
       end
 
       it "returns a specific Item" do
@@ -45,28 +48,23 @@ RSpec.describe 'items api', type: :request  do
         parsed_data = JSON.parse(response.body, symbolize_names: true)
 
         expect(parsed_data[:data].keys).to eq([:id, :type, :attributes])
-        expect(parsed_data[:data][:attributes].size).to eq(4)
-        expect(parsed_data[:data][:id]).to eq(Item.first.id.to_s)
+        expect(parsed_data[:data][:attributes].keys).to eq([:name, :description, :unit_price, :merchant_id])
+        expect(parsed_data[:data][:id]).to eq(@show_item.id.to_s)
         expect(parsed_data[:data][:type]).to eq("item")
-        expect(parsed_data[:data][:attributes][:name]).to eq(Item.first.name)
-        expect(parsed_data[:data][:attributes][:description]).to eq(Item.first.description)
-        expect(parsed_data[:data][:attributes][:unit_price]).to eq(Item.first.unit_price)
-        expect(parsed_data[:data][:attributes][:merchant_id]).to eq(Item.first.merchant_id)
+        expect(parsed_data[:data][:attributes][:name]).to eq(@show_item.name)
+        expect(parsed_data[:data][:attributes][:description]).to eq(@show_item.description)
+        expect(parsed_data[:data][:attributes][:unit_price]).to eq(@show_item.unit_price)
+        expect(parsed_data[:data][:attributes][:merchant_id]).to eq(@show_item.merchant_id)
       end
     end
 
     context "when unsuccessful" do
-
-      before do
-        get "/api/v1/items/1a2d3"
-      end
-
       it "returns an error message" do 
-
-        expect(response).to have_http_status(404)
-
+        get "/api/v1/items/1a2d3"
+        
         parsed_data = JSON.parse(response.body, symbolize_names: true)
-
+        
+        expect(response).to have_http_status(404)
         expect(parsed_data[:message]).to eq("Couldn't find Item with 'id'=1a2d3")
       end
     end
@@ -77,27 +75,33 @@ RSpec.describe 'items api', type: :request  do
     context "when successful" do
 
       it "create a new item" do
-      
+        
         item_params = ({
           name: 'Big thing',
           description: 'Its not a small thing',
           unit_price: 1000.99,
           merchant_id: Merchant.first.id
         })
-
+        
         headers = {"CONTENT_TYPE" => "application/json"}
-
+          
         post "/api/v1/items/", headers: headers, params: JSON.generate(item: item_params)
-
+          
+        item_new = Item.last
+      
+        expect(response).to be_successful
         expect(response).to have_http_status(201)
 
         parsed_data = JSON.parse(response.body, symbolize_names: true)
-
-        item_new = Item.last
         
+        expect(parsed_data[:data].keys).to eq([:id, :type, :attributes])
+        expect(parsed_data[:data][:attributes].keys).to eq([:name, :description, :unit_price, :merchant_id])
+        expect(parsed_data[:data][:id]).to eq(item_new.id.to_s)
+        expect(parsed_data[:data][:type]).to eq("item")
         expect(parsed_data[:data][:attributes][:name]).to eq(item_new[:name])
         expect(parsed_data[:data][:attributes][:description]).to eq(item_new[:description])
         expect(parsed_data[:data][:attributes][:unit_price]).to eq(item_new[:unit_price])
+        expect(parsed_data[:data][:attributes][:unit_price]).to be_a(Float)
         expect(parsed_data[:data][:attributes][:merchant_id]).to eq(item_new[:merchant_id])
       end 
     end
@@ -111,7 +115,6 @@ RSpec.describe 'items api', type: :request  do
   describe "#delete" do
 
     context "when successful" do
-
       it " deletes an item " do 
         item = Item.last
 
@@ -120,9 +123,9 @@ RSpec.describe 'items api', type: :request  do
         delete "/api/v1/items/#{item.id}"
 
         expect(response).to be_successful
+        expect(response).to have_http_status(204)
 
         expect(Item.count).to eq(2)
-        expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
         expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
