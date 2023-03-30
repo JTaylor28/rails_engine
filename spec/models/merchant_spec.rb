f377fed517c7b92afc2c9c123b88112d0edc6d37
James Taylor <jrtaylor0614@gmail.com>
2023-03-30 15:54:58 -0600

    Add class method ::seach_merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe Merchant, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name}
   end
+
+  describe "class methods " do 
+    describe "::search_merchant" do
+      it " returns the first merchant object by name alphabetically " do
+        merchant_1 = create(:merchant, name: "Chris Smith")
+        merchant_2 = create(:merchant, name: "Christopher Taylor")
+        merchant_3 = create(:merchant, name: "John Mcdanial ")
+        merchant_4 = create(:merchant, name: "Jonathan parks ")
+
+        expect(Merchant.search_merchant("chr")).to eq(merchant_1)
+        expect(Merchant.search_merchant("chr")).to_not eq(merchant_2)
+      end
+    end
+  end
 end
