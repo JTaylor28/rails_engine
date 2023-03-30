f8fa41a1e12ee36bda21e017fb777c1c1fe7d88d
James Taylor <jrtaylor0614@gmail.com>
2023-03-30 16:04:06 -0600

    refactor: makes files plural

---
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Merchants::ItemsController < ApplicationController
+  
+  def index
+    item = Merchant.find(params[:merchant_id]).items
+    render json: ItemSerializer.new(item)
+  end
+
+end
